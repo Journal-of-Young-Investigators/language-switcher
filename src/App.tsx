719238f9ba6d39ab8cb4a32cc4c2ce181c4d1fd8
U-DESKTOP-OJ6PD5N\kubiv <jakub.vogel@jyi.org>
2021-08-18 13:58:02 -0400

    Added more specificity to the jyiArticleContent interface adding title, date, authors, translators, and body. Modified the output to accomodate this and also played with some more CSS styling.

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ interface jyiArticleContent {
   // See list here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
   // You don't have to store display name of language ('English' for 'en' for example), get it using this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DisplayNames/DisplayNames
   langCode: keyof typeof Language;
-  // Text of the article in the language specified.
-  // Don't worry about the format of the text - just render it as it is given.
-  text: string;
+  title: string;
+  date: string;
+  authors: string[];
+  translators?: string[];
+  body: string;
 };
 
 interface jyiArticle {
@@ -42,13 +44,23 @@ class LanguageSwitcher extends React.Component<MyProps, MyState> {
   handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
     this.setState({ currentLang: event.currentTarget.lang });
   };
+
+  formatAuthorsOrTranslators = (authors: string[]) => {
+    let formatted: string = '';
+    for (let i = 0; i < authors.length; i++) {
+      formatted += authors;
+      if (i < authors.length-1) formatted += ', ';
+    }
+    
+    return formatted;
+  }
   
   render() {
     return (
       <div>
         <div className="lang-options">
           {
-            this.state.article.content.map( ( content: jyiArticleContent ) => {
+            this.state.article.content.map( (content: jyiArticleContent) => {
               return (
               <div className="lang-option" lang={content.langCode} onClick={this.handleClick} current-lang={this.state.currentLang === content.langCode ? "true" : "false"}>
                 {Language[content.langCode]}
@@ -57,9 +69,20 @@ class LanguageSwitcher extends React.Component<MyProps, MyState> {
             })
           }
         </div>
-        <div className="article-text">
+        <div className="article-content">
           {this.state.article.content.map( ( content: jyiArticleContent ) => {
-            return (this.state.currentLang === content.langCode) ? content.text : "";
+            if (this.state.currentLang === content.langCode) {
+              return (
+                <>
+                  <h2>{content.title}</h2>
+                  <div className="date">{content.date}</div>
+                  <h3>{this.formatAuthorsOrTranslators(content.authors)}</h3>
+                  <h3>{content.translators != null ? this.formatAuthorsOrTranslators(content.translators): 'No translator'}</h3>
+                  <p>{content.body}</p>
+                </>
+              );
+            }
+            else return "";
           })}
         </div>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 interface jyiArticleContent {
   // ISO 639 code to identify language; 'en' for English, 'es' for Spanish, etc.
+  // See list here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
   // You don't have to store display name of language ('English' for 'en' for example), get it using this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DisplayNames/DisplayNames
   langCode: keyof typeof Language;
-  // Text of the article in the language specified.
-  text: string;
+  title: string;
+  date: string;
+  authors: string[];
+  translators?: string[];
+  body: string;
 };
 
 interface jyiArticle {
@@ -25,10 +29,12 @@ enum Language {
 };
 
 let testjyiArticleContent: jyiArticleContent[] = [
-  {langCode: 'en', text: 'This is an example of text in English'}, 
-  {langCode: 'es', text: 'Este es un ejemplo de texto en español'},
-  {langCode: 'zh', text: '这是中文汉字。'},
-  {langCode: 'fr', text: 'Baguette'},
+  {langCode: 'en', title: 'Nanoparticles: the Future of Drug Delivery', date: 'January 1, 2020', authors: ['Syed Haider'], 
+    body: 'Drug development is one of the largest industries in the world. A significant amount of resources and time is invested into to produce drugs for clinical use. Equally important is the process of drug delivery and targeting by which the synthesized compounds are delivered to tissues in the body. As reported by Transparency Market Research in 2018, the drug delivery market is rapidly rising and is expected to be worth US $900 billion by 2025.'}, 
+  {langCode: 'es', title: 'Nanopartículas: el futuro de la administración de fármacos', date: '1 de enero, 2020', authors: ['Syed Haider'], translators: ['Carina Brizuela'], 
+    body: 'El desarrollo de medicamentos es una de las mayores industrias del mundo. Se invierte una gran cantidad de recursos y tiempo en la producción de fármacos para su uso clínico. Igual de importante es la administración dirigida de fármacos, proceso mediante el cual los compuestos sintetizados llegan a los tejidos del cuerpo. Como informó Transparency Market Research en 2018, el mercado de la administración de medicamentos está aumentando rápidamente y se espera que tenga un valor de 900 mil millones de dólares para el 2025.'},
+  {langCode: 'zh', title: '这是中文汉字。', date: 'date in chinese', authors: ['Syed Haider'], body: 'chinese body text'},
+  {langCode: 'fr', title: 'Baguette', date: 'date in french', authors: ['Syed Haider'], body: 'french body'}
 ]
 
 let testjyiArticle: jyiArticle = {
